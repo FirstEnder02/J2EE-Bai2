a320112d700057b82f43e03cdb2a38aa7cac9396
Huy <tranhauhuy265@gmail.com>
2026-02-03 13:06:19 +0700

    initial

---
+++ src/main/java/Springtrap/SpringtrapApplication.java
@@ -0,0 +1,13 @@
+package Springtrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtrapApplication.class, args);
+	}
+
+}
